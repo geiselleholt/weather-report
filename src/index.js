203b26482ca09de4c472db1bbaa464e694b0149b
Geiselle Holt <geiselleholt@hotmail.com>
2022-12-07 18:11:37 -0700

    Wave 1 and 2 update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 'use strict';
 
+const state = {
+  tempValue: 70,
+};
+
+const increaseTemp = () => {
+  state.tempValue += 1;
+  const increaseTempControl = document.querySelector("#tempValue")
+  increaseTempControl.textContent = `${state.tempValue}`;
+  console.log('anything')
+};
+
+const decreaseTemp = () => {
+  state.tempValue -= 1;
+  const decreaseTempControl = document.querySelector("#tempValue")
+  decreaseTempControl.textContent = `${state.tempValue}`;
+};
+
+const registerEventHandlers = () => {
+  const upButton = document.querySelector("#increaseTempControl");
+  upButton.addEventListener("click", increaseTemp);
+
+  const downButton = document.querySelector("#decreaseTempControl");
+  downButton.addEventListener("click", decreaseTemp);
+
+};
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
